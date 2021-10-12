c2d68c4e52a318839a36da1af9a037c0917eb0e3
marqdevx <p.marquinez@arduino.cc>
2021-10-12 15:27:24 +0200

    Add v8 examples

---
+++ libraries/LittleVGL/examples/v7/Portenta_lvgl/Portenta_lvgl.ino
@@ -0,0 +1,19 @@
+#include "Portenta_LittleVGL.h"
+#include "lv_demo_widgets.h"
+
+void setup() {
+  // put your setup code here, to run once:
+  portenta_init_video();
+
+  /*Hell world label*/
+  //lv_obj_t * label = lv_label_create(lv_scr_act(), NULL);
+  //lv_label_set_text(label, "Hello Arduino! Dev-7,0");
+  //lv_obj_align(label, NULL, LV_ALIGN_CENTER, 0, 0);
+
+  lv_demo_widgets();
+}
+
+void loop() {
+  lv_task_handler();
+  delay(3);
+}
